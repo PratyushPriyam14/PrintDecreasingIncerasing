3553008fb750acc8037e58509657ae9f3f7a6e4d
Pratyush <72009424+PratyushPriyam14@users.noreply.github.com>
2021-01-14 01:46:09 +0530

    Add files via upload

---
+++ PDI.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class PDI
+{
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pdi(n);
+
+    }
+    public static void pdi(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        pdi(n-1);
+        System.out.println(n);
+    }
+}
